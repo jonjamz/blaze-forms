5cc64836447ae28630524721ff00433391b81170
Jonathan James <jonjamz86@gmail.com>
2014-12-25 09:44:34 -0800

    up version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'templates:forms',
   summary: 'Dead easy reactive forms with validation.',
-  version: '1.1.4',
+  version: '1.1.5',
   git: 'https://github.com/meteortemplates/forms.git'
 });
 
