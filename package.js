e1cf9b465b8504e08ede92b6874106c7e8ecf71c
Jon James <jon@threering.com>
2015-01-15 09:46:59 -0500

    #5 instructions field should work on client and server

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'templates:forms',
   summary: 'Dead easy reactive forms with validation.',
-  version: '1.6.1',
+  version: '1.6.2',
   git: 'https://github.com/meteortemplates/forms.git'
 });
 
@@ -20,7 +20,7 @@ Package.onUse(function(api) {
   api.use('aldeed:simple-schema@1.2.0');
   api.imply('aldeed:simple-schema');
 
-  api.addFiles('simple-schema.coffee', 'client');
+  api.addFiles('simple-schema.coffee', ['client', 'server']);
   api.addFiles('templates:forms.coffee', 'client');
   api.addFiles('templates:forms.html', 'client');
 });
